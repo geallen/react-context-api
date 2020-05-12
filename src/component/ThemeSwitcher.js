58184f3e6acd575b09d8910d68e222a5405c5649
geallen <gamze.sen3827@gmail.com>
2020-05-12 15:12:11 +0300

    context api example

---
+++ src/component/ThemeSwitcher.js
@@ -0,0 +1,14 @@
+import React, {useContext} from 'react';
+import ThemeContext from '../context/ThemeContext';
+
+const ThemeSwitcher = () => {
+    const {theme, toggleTheme} = useContext(ThemeContext);
+
+    return (
+        <button onClick={toggleTheme} >
+            {theme === 'dark' ? <span>☀️</span> : <span>🌙</span>}
+        </button>
+    )
+}
+
+export default ThemeSwitcher;
